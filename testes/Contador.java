3313df42672a7daafd4eba5a28a1e6aa2bf15a6e
Gustavo Rebelo <141068211+Guglevers@users.noreply.github.com>
2024-09-25 01:01:19 +0000

    estudos

---
+++ testes/Contador.java
@@ -0,0 +1,16 @@
+public class Contador { //aqui temos uma classe, ou seja um molde da nossa peça, 
+
+    //public = indica que essa classe pode ser acessada por outras partes do programa
+    //class = indica a criação de uma classe
+    //contador = é o nome da classe
+    public static void main(String[] args) { //aqui temos um 'método'
+
+    //static = tudo citado nesta classe estara dentro dela
+    //void = ela ira retornar nada, um vazio, void
+    //main indica que é o método principal do programa
+    //"String[] args = serve para evitar erros com compiladores de java"
+        System.out.println("oi");
+        
+    }
+}
+
