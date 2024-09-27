942ca311089962a0723691d886cab62e287cb135
Gustavo Rebelo <141068211+Guglevers@users.noreply.github.com>
2024-09-27 00:19:24 +0000

    estudos

---
+++ testes/Pecalego.java
@@ -0,0 +1,6 @@
+public class Pecalego {
+    public static void main(String[] args) {
+        Pecalego minhapeca = new Pecalego();
+        System.out.println("Uma peça lego foi criada");
+    }
+}
