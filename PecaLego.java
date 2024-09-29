f7e24421d7bdb14df292eff81532447b6472899b
Ramon Mayor Martins <rmayormartins@gmail.com>
2024-09-29 13:14:42 +0000

    go

--- PecaLego.java
+++ PecaLego.java
@@ -1,17 +1,45 @@
 public class PecaLego {
-    // Atributo (característica da peça)
+    // Atributos (características da peça)
+    String cor;
     int numeroDePinos;
+    String formato;
+    boolean transparente;
+
+    // Métodos (ações que a peça pode realizar)
+    public void mostrarDetalhes() {
+ System.out.printf("Peça Lego %s com %d pinos, formato %s, %s transparente.\n", cor, numeroDePinos, formato, transparente ? "é" : "não é");
+    }
+
+    public void conectar() {
+        System.out.println("Peça " + cor + " conectada a outra peça!");
+    }
+
+    public void empilhar(int quantidade) {
+        System.out.println("Empilhadas " + quantidade + " peças " + cor + ".");
+    }
 
     public static void main(String[] args) {
-        // Criando um objeto (uma peça Lego real)
-        PecaLego minhaPeca = new PecaLego();
+        // Criando objetos (peças Lego reais)
+        PecaLego pecaVermelha = new PecaLego();
+        PecaLego pecaAzul = new PecaLego();
 
-        // Definindo a característica da peça
-        minhaPeca.numeroDePinos = 4;
+        // Definindo características da peça vermelha
+        pecaVermelha.cor = "Vermelha";
+        pecaVermelha.numeroDePinos = 4;
+        pecaVermelha.formato = "Retangular";
+        pecaVermelha.transparente = false;
 
-        // Mostrando a característica da peça
-        System.out.printf("Minha peça Lego tem " + minhaPeca.numeroDePinos + " pinos");
-        System.out.printf("Minha peça Lego tem %d pinos \n", minhaPeca.numeroDePinos);
+        // Definindo características da peça azul
+        pecaAzul.cor = "Azul";
+        pecaAzul.numeroDePinos = 2;
+        pecaAzul.formato = "Quadrado";
+        pecaAzul.transparente = true;
+
+        // Fazendo as peças realizarem ações
+        pecaVermelha.mostrarDetalhes();
+        pecaAzul.mostrarDetalhes();
+
+        pecaVermelha.conectar();
+        pecaAzul.empilhar(3);
     }
 }
-
--- ZZPecaLego.java
+++
@@ -1,6 +0,0 @@
-public class ZZPecaLego {
-    public static void main(String[] args) {
-        ZZPecaLego minhaPeca = new ZZPecaLego();
-        System.out.println("Uma peça Lego foi criada!");
-    }
-}
