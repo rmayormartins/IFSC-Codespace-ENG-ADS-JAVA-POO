6e5199059dc3b60439b38f7503e588fcaa5f1711
Ramon Mayor Martins <rmayormartins@gmail.com>
2024-10-09 00:21:15 +0000

    gogo

---
+++ B1_1Teste.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class B1_1Teste {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+       
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+       
+        System.out.println("Você digitou: " + numero);
+       
+        scanner.close();
+    }
+}
