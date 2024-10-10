d36c573d3483671b1accab273ec95636e9b1ccf3
Ramon Mayor Martins <rmayormartins@gmail.com>
2024-10-10 13:06:13 +0000

    go

---
+++ B1_7Teste.java
@@ -0,0 +1,10 @@
+public class B1_7Teste {
+    public static void main(String[] args) {
+        // Trabalhando com Character
+        String palavra = "teste";
+        System.out.println("Maiúsculo: " + palavra.toUpperCase());
+
+    }
+
+}
+
