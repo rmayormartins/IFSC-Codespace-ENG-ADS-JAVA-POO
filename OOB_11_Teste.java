3bbdd616809bdd7b3f017e313e9a9351a745da77
Ramon Mayor Martins <rmayormartins@gmail.com>
2024-11-07 00:20:36 +0000

    go

--- OOB_11_Teste.java
+++ OOB_11_Teste.java
@@ -6,7 +6,8 @@ public class OOB_11_Teste {
         }
     
         public static void main(String[] args) {
-            imprimirNumeros(1, 2, 3, 4, 5); // Imprime todos os números
+            imprimirNumeros(1, 2, 3, 4, 5); // passa varios números
+            imprimirNumeros(10); //passa um unico numero
         }
     }
     
---
+++ OOB_12_Teste.java
@@ -0,0 +1,16 @@
+public class OOB_12_Teste {
+    public static void dividir(int a, int b) {
+        try {
+            int resultado = a / b;
+            System.out.println("Resultado: " + resultado);
+        } catch (ArithmeticException e) {
+            System.out.println("Erro: divisão por zero não é permitida.");
+        }
+    }
+
+    public static void main(String[] args) {
+        dividir(10, 2); // Sem erro
+        dividir(10, 0); // Gera uma exceção
+    }
+}
+
