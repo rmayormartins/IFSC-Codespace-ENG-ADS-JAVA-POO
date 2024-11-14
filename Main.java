8990545d733061e68355aed7e32c75a1b7eda4c4
Ramon Mayor Martins <rmayormartins@gmail.com>
2024-11-14 00:18:02 +0000

    go

--- Main.java
+++ Main.java
@@ -1,33 +1,35 @@
 // Classe Motor
-public class Motor {
+class Motor {
     int potencia; // Potência do motor
+    String tipoCombustivel; // Tipo de combustível
+
+    void mostrarDetalhesMotor() {
+        System.out.println("Potência do motor: " + potencia + " cavalos");
+        System.out.println("Tipo de combustível: " + tipoCombustivel);
+    }
 }
 
 // Classe Carro
-public class Carro {
+class Carro {
     String marca; // Marca do carro
     Motor motor;  // O carro tem um motor
-
-    // Método para mostrar os detalhes do carro
     void mostrarDetalhes() {
         System.out.println("Marca do carro: " + marca);
-        System.out.println("Potência do motor: " + motor.potencia + " cavalos");
+        motor.mostrarDetalhesMotor();
     }
 }
 
 // Método principal (main)
 public class Main {
     public static void main(String[] args) {
-        // Criando o motor e definindo a potência
         Motor motorDoCarro = new Motor();
         motorDoCarro.potencia = 100;
+        motorDoCarro.tipoCombustivel = "Gasolina";
 
-        // Criando o carro, definindo a marca e associando o motor
         Carro meuCarro = new Carro();
         meuCarro.marca = "Fiat";
         meuCarro.motor = motorDoCarro;
 
-        // Mostrando os detalhes do carro
         meuCarro.mostrarDetalhes();
     }
 }
